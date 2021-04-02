cad2427bb911352a69b5ff936147e7e0a77436c3
Z-Rna <wadczyniciastek@gmail.com>
2021-04-02 02:29:34 +0200

    checked PI4.cpp

--- OMP1/PI4.cpp
+++ OMP1/PI4.cpp
@@ -7,16 +7,11 @@ double step;
 
 int main(int argc, char* argv[])
 {
-
-	int threads = 8;
-	omp_set_num_threads(threads);
-
-	clock_t start, stop;
+	omp_set_num_threads(8);
 	double start_omp, stop_omp;
 	double x, pi, sum = 0.0;
 	int i;
 	step = 1. / (double)num_steps;
-	//start = clock();
 	start_omp = omp_get_wtime();
 #pragma omp parallel
 	{
@@ -33,13 +28,9 @@ int main(int argc, char* argv[])
 	}
 
 	pi = sum * step;
-	//stop = clock();
 	stop_omp = omp_get_wtime();
 
 	printf("Wartosc liczby PI wynosi %15.12f\n", pi);
-	//printf("Czas przetwarzania wynosi %f sekund\n", \
-		((double)(stop - start) / 1000.0));
-	printf("Rzeczywisty czas przetwarzania wynosi %f sekund\n", \
-		((double)(stop_omp - start_omp) ));
+	printf("Rzeczywisty czas przetwarzania wynosi %f sekund\n", (stop_omp - start_omp));
 	return 0;
 }
