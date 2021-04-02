cfd778aa5a472b47ec05afca8d07df2124643863
Z-Rna <wadczyniciastek@gmail.com>
2021-04-02 02:27:04 +0200

    checked code

--- OMP1/PI3.cpp
+++ OMP1/PI3.cpp
@@ -7,26 +7,31 @@ double step;
 
 int main(int argc, char* argv[])
 {
-
-	omp_set_num_threads(8);
+	omp_set_num_threads(4);
 
 	double start_omp, stop_omp;
 	double x, pi, sum = 0.0;
 	int i;
 	step = 1. / (double)num_steps;
 	start_omp = omp_get_wtime();
-#pragma omp parallel for
-	for (i = 0; i < num_steps; i++)
+#pragma omp parallel
 	{
-		double x = (i + .5) * step;
+		double sum1 = 0;
+#pragma omp for
+		for (i = 0; i < num_steps; i++)
+		{
+			double x = (i + .5) * step;
+
+			sum1 += 4.0 / (1. + x * x);
+		}
 #pragma omp atomic
-		sum += 4.0 / (1. + x * x);
+		sum += sum1;
 	}
 
 	pi = sum * step;
 	stop_omp = omp_get_wtime();
 
 	printf("Wartosc liczby PI wynosi %15.12f\n", pi);
-	printf("Rzeczywisty czas przetwarzania wynosi %f sekund\n", ((double)(stop_omp - start_omp)));
+	printf("Rzeczywisty czas przetwarzania wynosi %f sekund\n", (stop_omp - start_omp));
 	return 0;
 }
--- OMP1/PI6.cpp
+++ OMP1/PI6.cpp
@@ -8,7 +8,7 @@ double step;
 int main(int argc, char* argv[])
 {
 
-	const int threads = 2;
+	const int threads = 8;
 	omp_set_num_threads(threads);
 
 	clock_t start, stop;
@@ -16,7 +16,6 @@ int main(int argc, char* argv[])
 	double x, pi, sum = 0.0, tab[threads];
 	int i;
 	step = 1. / (double)num_steps;
-	//start = clock();
 	start_omp = omp_get_wtime();
 #pragma omp parallel
 	{
@@ -35,13 +34,9 @@ int main(int argc, char* argv[])
 	}
 
 	pi = sum * step;
-	//stop = clock();
 	stop_omp = omp_get_wtime();
 
 	printf("Wartosc liczby PI wynosi %15.12f\n", pi);
-	//printf("Czas przetwarzania wynosi %f sekund\n", \
-		((double)(stop - start) / 1000.0));
-	printf("Rzeczywisty czas przetwarzania wynosi %f sekund\n", \
-		((double)(stop_omp - start_omp) ));
+	printf("Rzeczywisty czas przetwarzania wynosi %f sekund\n", (stop_omp - start_omp));
 	return 0;
 }
